bc0c6a0f66dac5b91ef544a946ec8c866e187af4
Vallabh Anwikar <vranwikar@gmail.com>
2025-07-13 19:20:52 +0530

    First commit

---
+++ BasicDataStructures/Operators.py
@@ -0,0 +1,18 @@
+print('Addition: ', 100 + 2)
+print('Subtraction: ', 100 - 1)
+print('Multiplication: ', 2 * 3)
+print ('Division: ', 9 / 2)
+print('Division: ', 5 / 2)
+print('Division: ', 1 / 2)
+print('Division without the remainder: ', 7 // 2)
+print ('Division without the remainder: ',7 // 3)
+print('Modulus: ', 3 % 2)
+print('Exponentiation: ', 2 ** 3)
+
+print('Complex number: ', 1 + 1j)
+print('Multiplying complex numbers: ',(1 + 1j) * (1 - 1j))
+
+print("Enter your age ")
+age = int(input(" "))
+seconds = 365 * age * 24 * 60 * 60
+print("seconds lived ", seconds)
---
+++ BasicDataStructures/Variables.py
@@ -0,0 +1,14 @@
+
+firstname = "Vallabh"
+lastname = "Anwikar"
+fname = firstname + " " + lastname
+
+print(firstname)
+print(lastname)
+print(fname)
+
+testList = [1, 2, 3, 4, 5]
+sum = 0
+for i in testList:
+    sum += i
+print(sum)
